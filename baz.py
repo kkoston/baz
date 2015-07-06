7c3988ba0f8ecd6ac693ad63b6d9a749ac83ccf4
Chris <chris@koston.us>
2015-07-06 17:01:21 -0400

    Abc

--- baz.py
+++ baz.py
@@ -10,9 +10,26 @@ ads = soup.find_all('ul', id=re.compile('id\d+'))
 for ad in ads:
     ad_id = ad.attrs['id'].replace('id', '')
     adt = ad.getText()
-    adt = re.sub(r'\s+', ' ', adt).replace('Lokal do Wynajecia', '').replace(' - ', '\n').strip()
+    adt = re.sub(r'\s+', ' ', adt).replace('Lokal do Wynajecia', '').strip()
     if not adt[0].isdigit():
         continue
+        
+    date = adt[:10]
+    adt = adt[13:]
+    
+    price = ''
+    price = re.search(r'cena \$: (\d+)', adt)
+    if price != None:
+        price = price.group(1)
+    
+    name = ''    
+    name = re.search(r'name: ([^\s]+)', adt)
+    if name != None:
+        name = name.group(1)        
+    
+    
+    adt = re.sub(r'<more>.*', '', adt)
+    print date, name, price
     print adt
     print 'http://bazarynka.com/ogloszenia/telefon/%s/ktory/1' % ad_id
     print
